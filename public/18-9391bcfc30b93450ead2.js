3d5c8d67a7033cbb4be1cf525c2a30b71f2467b8
yusufu <unlu.yufus@gmail.com>
2019-09-22 19:37:05 +0300

    added public folder to publish

---
+++ public/18-9391bcfc30b93450ead2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{249:function(n,w,o){}}]);
+//# sourceMappingURL=18-9391bcfc30b93450ead2.js.map
---
+++ public/5-2d97edd675cd53d3d355.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{245:function(n,w,o){}}]);
+//# sourceMappingURL=5-2d97edd675cd53d3d355.js.map
