659eb577db5fbfc40ae40496449b19b6bbec4594
yusufu <yusuf.unlu@artiwise.com>
2020-03-28 20:54:36 +0300

    removed all pages and add mines, removed public folder from git

--- public/3-cbfe5118c084928d0049.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{246:function(n,w,o){}}]);
-//# sourceMappingURL=3-cbfe5118c084928d0049.js.map
--- public/6-cda4de96f3f157bf68eb.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{247:function(n,w,o){}}]);
-//# sourceMappingURL=6-cda4de96f3f157bf68eb.js.map
