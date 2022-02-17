c87818eb142b080303b9c84062c448c50e223e47
yusufu <yusuf.unlu@bitlo.com>
2022-02-17 00:39:04 -0500

    username fixes and bio

--- src/components/statics.js
+++ src/components/statics.js
@@ -1,5 +1,6 @@
 export const STATICS = {
   owner: 'Yusuf Unlu',
-  ownerTwitter: 'https://twitter.com/yufusunlu',
+  ownerTwitter: 'https://twitter.com/yufusunlu-deneme3',
   ownerGithub: 'https://github.com/yusufunlu',
+  ownerLinkedin: 'https://www.linkedin.com/in/yusufunlu/',
 };
